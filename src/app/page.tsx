141d9611fbdfc353d755dd1e45e404cf8da21e91
Muriel <eesneider2118@gmail.com>
2025-03-19 01:46:04 -0500

    faltan 15 pa las 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,6 @@ import { useAuth } from "../context/AuthContext";
 import Landing from "../app/landing/page";
 
 export default function Home() {
-  const {user, logOut } = useAuth();
-
-  if (!user) {
-    return <Landing/>
-  }
-  
-  return (
-    <div>
-    </div>
-  );
+  return <Landing />;
 }
+
